d0c4d835b62fe5dd6789b3dbacb1ad9989c8cc07
IWANABETHATGUY <iwanabethatguy@qq.com>
2023-04-08 21:22:02 +0800

    feat

---
+++ install.fish
@@ -0,0 +1,13 @@
+if not set -q argv[1]
+    echo "Plugins directory not specified, please pass your zsh plugins directory as a parameter."
+    exit 1
+end
+
+if test -d $argv[1]
+    mkdir -p ~/.config/fish/completions/
+    cp ./pnpm-shell-completion.fish ~/.config/fish/completions/pnpm.fish
+    cp ./pnpm-shell-completion $1/pnpm-shell-completion
+else
+    echo "$argv[1] is not exist"
+end
+
---
+++ pnpm-shell-completion.fish
@@ -0,0 +1,76 @@
+set -g filter_flag
+
+set -g __fish_pnpm_cmdline
+set -g __fish_pnpm_remove_cmdline
+
+complete -c pnpm -f
+
+complete -c pnpm -n "not __fish_seen_subcommand_from remove" -a "(__get_string)"
+
+complete -c pnpm  -f -l filter -r -a "$(FEATURE=filter pnpm-shell-completion)"
+# add relative args
+complete -c pnpm -n "not __fish_use_subcommand"  -f -a "add"
+complete -c pnpm -n __could_add_global -l global -s g
+complete -c pnpm -n "__fish_seen_subcommand_from add" -l save-dev -s D
+complete -c pnpm -n "__fish_seen_subcommand_from add" -l save-peer
+
+
+
+
+complete -c pnpm -n "__fish_seen_subcommand_from remove" -f -a "(__get_remove)"
+
+
+
+
+function __get_string
+  set -l cmdline "$(commandline -c)"
+  if set -q __fish_pnpm_cmdline; and test "$cmdline" = "$__fish_pnpm_cmdline"
+    return 0
+  end
+
+  set -g __fish_pnpm_cmdline $cmdline
+
+  set -l tokens (commandline -opc)
+  set -e tokens[1] # assume the first token is `pnpm`
+  argparse filter= -- $tokens 2>/dev/null
+  TARGET_PKG=$_flag_filter FEATURE=scripts pnpm-shell-completion
+
+end
+
+function __get_remove
+  set -l cmdline "$(commandline -c)"
+  if set -q __fish_pnpm_remove_cmdline; and test "$cmdline" = "$__fish_pnpm_remove_cmdline"
+    return 0
+  end
+
+  set -g __fish_pnpm_remove_cmdline $cmdline
+
+  set -l tokens (commandline -opc)
+  set -e tokens[1] # assume the first token is `pnpm`
+  argparse filter= -- $tokens 2>/dev/null
+  TARGET_PKG=$_flag_filter FEATURE=deps pnpm-shell-completion
+
+end
+
+function __has_filter
+  set -l tokens (commandline -opc)
+  set -e tokens[1] # assume the first token is `pnpm`
+  argparse filter= -- $tokens 2>/dev/null
+  if count $_flag_filter == 0
+    return 1
+  end
+  return 0
+end
+
+
+# predicate function, 1 means false, 0 means true
+function __could_add_global
+  if __has_filter == 0
+    return 1
+  end
+  if __fish_seen_subcommand_from add == 0
+    return 0
+  end
+  return 1
+end
+
